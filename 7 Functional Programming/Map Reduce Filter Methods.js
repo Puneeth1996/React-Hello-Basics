cad3fd4aa2787a66e4a789376c3e0bf60e69783b
Puneeth P <puneeth1996p@gmail.com>
2019-06-03 08:08:33 +0530

    JS Map Reduce Filter Methods
    Understand how these three mehtods work , cause they help in making Working in react much easier when dealing states and data

---
+++ 7 Functional Programming/Map Reduce Filter Methods.js
@@ -0,0 +1,6 @@
+//Functional Programming map filter and reduce
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
